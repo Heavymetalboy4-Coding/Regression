980919352226bd41582270306c84a17dea0388b5
Heavymetalboy4-Coding <1000120@students.nishkamschools.org>
2024-12-08 16:51:30 +0000

    Add files via upload

--- titanic.py
+++ titanic.py
@@ -1,47 +1,37 @@
-import numpy as np
-import matplotlib.pyplot as plt 
-from sklearn.model_selection import train_test_split
-from sklearn import linear_model 
 import pandas as pd
+import matplotlib.pyplot as plt 
 from sklearn.linear_model import LogisticRegression
-from sklearn import preprocessing
-titanic = pd.read_csv("C:/Users/arjun/OneDrive/Documents/Programming/Data/DataScience/AI Model/titanic.csv")
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+from sklearn.decomposition import PCA
+from sklearn.model_selection import train_test_split
+
+titanic = pd.read_csv("C:/Users/arjun/OneDrive/Documents/Programming/Data/DataScience/KNN/titanic.csv")
 print(titanic.isnull().sum())
-#taking care of the null values
-print(titanic["Age"].median(skipna= True))
-titanic["Age"].fillna(titanic["Age"].median(skipna= True),inplace=True)
-print(titanic.isnull().sum())
-#print(titanic.head())
-
-#Dropping the unnecesary data
-titanic.drop("PassengerId", axis= 1, inplace=True)
-titanic.drop("Name", axis= 1, inplace=True)
-titanic.drop("Ticket", axis= 1, inplace=True)
-titanic.drop("SibSp", axis= 1, inplace=True)
-titanic.drop("Parch", axis= 1, inplace=True)
-titanic.drop("Cabin", axis= 1, inplace=True)
-
-#print(titanic.head())
-
-preprocessing_titanic = preprocessing.LabelEncoder()
-titanic["Sex"] = preprocessing_titanic.fit_transform(titanic["Sex"])
-titanic["Embarked"] = preprocessing_titanic.fit_transform(titanic["Sex"])
+titanic = titanic.drop(columns=["Name"])
+encoder = LabelEncoder()
+titanic["Sex"] = encoder.fit_transform(titanic["Sex"])
 print(titanic.head())
-X = titanic[["Pclass", "Sex", "Age", "Embarked"]]
+X = titanic.drop(columns=["Survived"])
 y = titanic["Survived"]
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 1)
-model = LogisticRegression()
-model.fit(X_train, y_train)
-ypredict = model.predict(X_test)
-from sklearn.metrics import classification_report,confusion_matrix
-matrix = confusion_matrix(y_test, ypredict)
-import seaborn as sns
+scalar = StandardScaler()
+XScaled = scalar.fit_transform(X)
+pca = PCA(n_components = 2)
+XScaled_pca = pca.fit_transform(XScaled)
+print(XScaled_pca)
+X_train, X_test, y_train, y_test = train_test_split(XScaled_pca, y, test_size=0.2, random_state= 1)
 
-sns.heatmap(matrix, annot=True, fmt="d")
-plt.xlabel("Predicted")
-plt.ylabel("Actual")
-plt.title("Matrix")
-plt.legend()
-plt.show()
-print(classification_report(y_test, ypredict))
+log_reg = LogisticRegression()
+log_reg.fit(X_train, y_train)
+y_pred= log_reg.predict(X_test)
+print(y_pred)
+
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+accuracy = accuracy_score(y_test, y_pred)
+print(accuracy)
+
+class_report = classification_report(y_test, y_pred)
+print(class_report)
+
+mat = confusion_matrix(y_test, y_pred)
+print(mat)
